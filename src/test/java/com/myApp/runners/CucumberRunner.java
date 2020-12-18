e12ab22c4d1d531da7e9d961ae4eb7b910f2a6db
altayozyavas <altayozyavas@gmail.com>
2020-12-18 17:10:38 -0600

    plugin update in cucumber runner

--- src/test/java/com/myApp/runners/CucumberRunner.java
+++ src/test/java/com/myApp/runners/CucumberRunner.java
@@ -7,9 +7,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "json:target/cucumber.json",
         features = "src/test/resources/features",
         glue = "com/myApp/step_definitions",
-        dryRun = true
+        dryRun = false
 )
 
 public class CucumberRunner {
