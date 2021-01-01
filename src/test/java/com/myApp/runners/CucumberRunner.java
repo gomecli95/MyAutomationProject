f2dd05660911348d291f6b246a5551327b3b7dc5
altayozyavas <altayozyavas@gmail.com>
2021-01-01 13:50:33 -0600

    regression update

--- src/test/java/com/myApp/runners/CucumberRunner.java
+++ src/test/java/com/myApp/runners/CucumberRunner.java
@@ -12,8 +12,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/myApp/step_definitions",
-        dryRun = false
-        //, tags = "@regression"
+        dryRun = false,
+        tags = "@regression"
 )
 
 public class CucumberRunner {
